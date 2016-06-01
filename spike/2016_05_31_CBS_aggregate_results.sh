d754b6b46cdb0ef16ef40ddb155e42885ec391ee
jcharit1 <jimmy.charite@gmail.com>
2016-06-01 14:52:32 -0400

    New CBS Aggregation Bash Script

--- spike/2016_05_31_CBS_aggregate_results.sh
+++ spike/2016_05_31_CBS_aggregate_results.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 NAMEID=$1
+MAXTHREADS=$2
 SEARCHDIR=/NOBACKUP/scratch/jc4144/Nachum/Ostrich/cluster/
-OUTFILE=/NOBACKUP/scratch/jc4144/Nachum/Ostrich/temp_file_list.txt
+OUTFILE=/NOBACKUP/scratch/jc4144/Nachum/Ostrich/temp_file_list
 
 # Print a directory tree
-find $SEARCHDIR > $OUTFILE
+find $SEARCHDIR > $OUTFILE.txt
 
 # Isolate files that I need
-grep "$NAMEID" $OUTFILE > $OUTFILE
+grep "$NAMEID" $OUTFILE.txt > $OUTFILE.txt
 
 # Record number of files
-NUMFILES=$(wc -l $OUTFILE)
+NUMFILES=$(wc -l $OUTFILE.txt)
 
 # Loop over them and append to a new file
-for FILE in `seq 1 $NUMFILES`;
+for FILE in `seq 1 $MAXTHREADS`;
 do
-	cat $FILE >> temp_agg_results_$NAMEID.txt
+	cat $FILE >> $NAMEID.txt
 done
