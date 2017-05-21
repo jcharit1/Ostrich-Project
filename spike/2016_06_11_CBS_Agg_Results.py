ee6c24a18e3a04188bc8639aab68bba12af7cbe6
jcharit1 <jimmy.charite@gmail.com>
2017-05-21 15:14:08 -0400

    changed bash code for collecting results

--- spike/2016_06_11_CBS_Agg_Results.py
+++ spike/2016_06_11_CBS_Agg_Results.py
@@ -4,12 +4,14 @@ import sys,os,fnmatch
 # Base name of the output file
 ########################################################################
 oname = str(sys.argv[1])
-bdir='/NOBACKUP/scratch/jc4144/Nachum/Ostrich/'
+outfile_name = str(sys.argv[2])
+
+bdir='/NOBACKUP/scratch/jc4144/Nachum/Ostrich/cluster/'
+outFile='/NOBACKUP/scratch/jc4144/Nachum/Ostrich/'+outfile_name+'.txt'
 
 # find and aggregate them into one file
 ########################################################################
 strMatch="*"+oname+"*"
-outFile=bdir+oname
 with open(outFile, "w") as fo:
 	for dirpath,dirnames,filenames in os.walk(bdir):
 		for files in filenames:
--- spike/2017_01_22_CBS_agg_results_p1.sh
+++ spike/2017_01_22_CBS_agg_results_p1.sh
@@ -7,6 +7,6 @@ BASENAME=$1
 FULL_PATH='/NOBACKUP/scratch/jc4144/Nachum/Ostrich/cluster/'
 for level_1 in $(ls $FULL_PATH);
 do
-   /apps/wrappers/sge_run --grid_quiet --grid_submit=batch --grid_mem=500M "find $FULL_PATH$level_1/ -name '*$BASENAME*' -exec cat {} + > /user/user1/jc4144/Nachum/Ostrich/AggOutput/temp_$level_1.txt"
+   /apps/wrappers/sge_run --grid_quiet --grid_submit=batch --grid_mem=500M "find $FULL_PATH$level_1/ -name '*$BASENAME*' -exec cat {} + > /user/user1/jc4144/Nachum/Ostrich/AggOutput/$BASENAME_$level_1.txt"
 done
 
